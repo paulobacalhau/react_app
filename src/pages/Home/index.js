f78c3fd98bf5661e5ab290710a16f4a62d10980b
Paulo Bacalhau <paulo.bacalhau@gmail.com>
2020-05-30 22:44:59 -0400

    Aplicativo completo de REACT - Gama Academy

---
+++ src/pages/Home/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+// import axios from '../node_modules/axios';
+import axios from 'axios';
+import * as S from './styled';
+import { useHistory }from 'react-router-dom';
+
+export default function Home(props) {
+  const history = useHistory();
+  const [ usuario, setUsuario ] = useState('');
+  const [ erro, setErro ] = useState(false);
+
+  function handlePesquisa(){
+    // console.log({usuario});
+    // axios.get(`https://api.github.com/users/ramosht/repos`).then(response => console.log(response.data));
+    // axios.get(`https://api.github.com/users/filipedeschamps/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+      .then(response => {
+        const repositories = response.data;
+        const repositoriesName = [];
+        repositories.map((repository) => {repositoriesName.push(repository.name)}
+        );
+        // console.log(JSON.stringify(repositoriesName));
+        localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+        setErro(false);
+        history.push('./repositories');
+    })
+      .catch(err=>{
+        setErro(true);
+      });
+  }
+  return (
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange = {e => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Content>
+      {erro ? <S.ErrorMessage>Ocorreu um erro! Tente novamente.</S.ErrorMessage>: ''}
+    </S.HomeContainer>
+  );
+}
+
